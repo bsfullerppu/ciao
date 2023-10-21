1b1fd42ee860a79153a1bb1c7158f818ee2d19d6
bsfuller <b.seth.fuller@gmail.com>
2023-10-21 10:40:20 -0400

    "App creaeted, added some content."

---
+++ src/components/ciao.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Ciao() {
+return <h1>Ciao!</h1>
+}
+
+export default Ciao;
---
+++ src/components/history.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+class History extends Component {
+    render() {
+        return <p>One of the first pieces of literature in the Italian language is Dante Alighieri's or simply Dante's Divine Comedy. </p>
+    }
+}
+export default History;
